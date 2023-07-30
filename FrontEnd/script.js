9cfeab1126965ad04333c5746abadb1d220edfe1
SaikinHub <syllasall@yahoo.fr>
2023-07-30 21:14:54 +0200

    Added "filter by category" feature

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,27 +1,79 @@
 const works = await fetch("http://localhost:5678/api/works").then(data => data.json());
 
-const portfolioContainer = document.getElementById("portfolio")
+const projectsContainer = document.getElementById("dynamic")
 
-function projectCardGenerator(array) {
-
-    const divElement = document.createElement("div");
+function listenerGenerator(categoryBtn) {
     
-    array.forEach(work => {
+    categoryBtn.addEventListener("click", () => {
+        if(categoryBtn.innerText === "Tous") {
+            // Runs the default behavior if the selected filter is "Tous"
+            listGenerator(works);
+        } else {
+            // Creates a custom list that matches the specifically selected filter and send it to the listGenerator
+            const customListArr = new Array();
+            works.forEach(work => {
+                if (categoryBtn.innerText === work.category.name) {
+                    customListArr.push(work)
+                }
+            })
+            listGenerator(customListArr);
+        }
+    })
+}
+
+function categoryGenerator() {
+    const categoryContainer = document.createElement("div");
+    categoryContainer.classList.add("categories");
+
+    // Creates a "Set" object in which each property cannot be added more than once
+    const categoriesSet = new Set();
+    categoriesSet.add("Tous");
+    
+    // Checks for all categories availlable and add them into the set
+    works.forEach(work => {
+        categoriesSet.add(work.category.name);
+    })
+
+    // Creates a DOM button for each category and add an eventListener to each one
+    categoriesSet.forEach(category => {
+        const btnElement = document.createElement("button");
+        btnElement.innerText = category;
+        listenerGenerator(btnElement);
+        categoryContainer.appendChild(btnElement);
+    }
+    )
+    return categoryContainer;
+}
+
+function listGenerator(array) {
+    const container = document.createElement("div");
+    container.classList.add("gallery")
+    
+    // Creates a formated list of projects off the given array 
+    array.forEach(item => {
         const figureElement = document.createElement("figure");
         const imgElement = document.createElement("img");
         const figcaptionElement = document.createElement("figcaption");
 
-        imgElement.src= work.imageUrl;
-        imgElement.alt= work.title
-        figcaptionElement.innerText = work.title
+        imgElement.src= item.imageUrl;
+        imgElement.alt= item.title;
+        figcaptionElement.innerText = item.title;
 
         figureElement.appendChild(imgElement);
         figureElement.appendChild(figcaptionElement);
-        divElement.appendChild(figureElement);
+        container.appendChild(figureElement);
     });
+    
+    // Clears the DOM container
+    projectsContainer.innerHTML = "";
+    
+    // Creates a formated list of button from all existing categories
+    const categories = categoryGenerator();
 
-    divElement.classList.add("gallery")
-    portfolioContainer.appendChild(divElement);
+    // Replace the content with the newly generated categories and projects lists
+    projectsContainer.appendChild(categories);
+    projectsContainer.appendChild(container);
+
 }
 
-projectCardGenerator(works);
+listGenerator(works);
